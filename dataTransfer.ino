3e138971f915b59acee1db9785c9f40595390ccd
SamSch <37340452+sschwarcz@users.noreply.github.com>
2021-08-09 14:12:42 +0300

    Add files via upload

--- dataTransfer.ino
+++ dataTransfer.ino
@@ -1,20 +1,17 @@
 
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
 // constants won't change:
-const long interval = 100;           // interval at which to blink (milliseconds)
-const long messageSpeed = 300;
+const long interval = 80;           // interval at which to blink (milliseconds)
+const long messageSpeed = 200;
 const long delayedTime = 5000;
 const long signalTime = 8000;
 const long timeOverTarget = 300000;
 String eOf = "01111111";
-String message = String("0100001001101111011000010111101000100000011010010111001100100000011101000110100001100101001000000110101101101001011011100110011100100001"+eOf);
-int package_size = 8;
+String message = String("010010000110010101101100011011000110111100100000010101110110111101110010011011000110010000100001"+eOf);
+int package_size = 8*5;
 bool sent = false;
 bool started = false;
 int count = 0;
